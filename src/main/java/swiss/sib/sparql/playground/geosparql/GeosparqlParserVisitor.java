5b1d910bb4da8767412c22b1607a5c1bcb33dd4d
RedCrusaderJr <37693301+RedCrusaderJr@users.noreply.github.com>
2021-03-21 17:37:57 +0100

    bug  fix

---
+++ src/main/java/swiss/sib/sparql/playground/geosparql/GeosparqlParserVisitor.java
@@ -0,0 +1,12 @@
+package swiss.sib.sparql.playground.geosparql;
+
+//import org.eclipse.rdf4j.query.parser.serql.ASTVisitorBase;
+//import org.eclipse.rdf4j.query.parser.sparql.ast.SyntaxTreeBuilderVisitor;
+
+public class GeosparqlParserVisitor {// xtends ASTVisitorBase//implements GeosparqlParserVisitor {
+
+	// public void visit() {
+	// //TODOs
+	// }
+
+}
