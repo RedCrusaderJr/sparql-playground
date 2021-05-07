52f5d3b9ba52d5de94273bcf3718fa41ca7faf84
RedCrusaderJr <37693301+RedCrusaderJr@users.noreply.github.com>
2021-05-07 02:49:53 +0200

    Formatting files

--- src/main/resources/static/js/app.directives.js
+++ src/main/resources/static/js/app.directives.js
@@ -1,91 +1,90 @@
-(function (angular, undefined) {'use strict';
+(function (angular, undefined) {
+	'use strict';
 
-/*
- * create snorql directives
- */
+	/*
+	* create snorql directives
+	*/
 
-angular.module('snorql.ui',[])
-  .directive("menuToggle",menuToggle)
-  .directive("sparqlFormatter",sparqlFormatter)
-  .filter("containsTag",containsTag)
-  .filter("addQueryPrefix",addQueryPrefix)
-  .filter("getGitHubUrl",getGitHubUrl);
+	angular.module('snorql.ui',[])
+		.directive("menuToggle",menuToggle)
+		.directive("sparqlFormatter",sparqlFormatter)
+		.filter("containsTag",containsTag)
+		.filter("addQueryPrefix",addQueryPrefix)
+		.filter("getGitHubUrl",getGitHubUrl);
 
-containsTag.$inject=[]
-function containsTag() {
-  return function( items, selectedTag) {
-    var filtered = [];
-    if(selectedTag == null)
-      return items;
+	containsTag.$inject=[]
+	function containsTag() {
+		return function( items, selectedTag) {
+			var filtered = [];
+			if(selectedTag == null)
+				return items;
 
-    angular.forEach(items, function(item) {
-      if(_.intersection([selectedTag], item.tags).length > 0) {
-        filtered.push(item);
-      }
-    });
-    return filtered;
-  };
-};
+			angular.forEach(items, function(item) {
+				if(_.intersection([selectedTag], item.tags).length > 0) {
+					filtered.push(item);
+				}
+			});
+			return filtered;
+		};
+	};
 
+	addQueryPrefix.$inject=[]
+	function addQueryPrefix() {
+		return function(queryId) {
+			var s = "000000000" + queryId;
+			return "NXQ_" + s.substr(s.length-5);
+		};
+	};
 
-addQueryPrefix.$inject=[]
-function addQueryPrefix() {
-  return function(queryId) {
-      var s = "000000000" + queryId;
-      return "NXQ_" + s.substr(s.length-5);
-    };
-};
+	getGitHubUrl.$inject=['config']
+	function getGitHubUrl(config) {
+		return function(queryId) {
+			var s = "000000000" + queryId;
+			var fileName = "NXQ_" + s.substr(s.length-5) + ".rq";
+			return config.githubEdit + fileName;
+		};
+	};
 
-getGitHubUrl.$inject=['config']
-function getGitHubUrl(config) {
-  return function(queryId) {
+	//
+	// implement menuToggle
+	menuToggle.$inject=[]
+	function menuToggle() {
+		function link(scope, element, attrs) {
+			element.click(function () {
+				$('.row-offcanvas').toggleClass('active')
+				//$('html').toggleClass('overvlow-hidden')
+			});
+		}
 
-    var s = "000000000" + queryId;
-    var fileName = "NXQ_" + s.substr(s.length-5) + ".rq";
-    return config.githubEdit + fileName;
-  };
-};
+		return {
+			link: link
+		};
+	}
 
-//
-// implement menuToggle
-menuToggle.$inject=[]
-function menuToggle() {
-  function link(scope, element, attrs) {
-    element.click(function () {
-      $('.row-offcanvas').toggleClass('active')
-      //$('html').toggleClass('overvlow-hidden')
-    });
-  }
-  return {
-    link: link
-  };
-}
+	//
+	// implement sparqlFormatter
+	sparqlFormatter.$inject=['snorql']
+	function sparqlFormatter(snorql) {
+		var formatter;
+		function link(scope, element, attrs) {
+			scope.$watch('sparqlFormatter', function(newValue, oldValue) {
+				if (newValue){
+					formatter= snorql.SPARQLResultFormatter()
+					element.html(formatter.toDOM())
+					element.find('a').click(function(){
+						window.scrollTop();
+					})
+				}
+			});
+		}
 
-
-//
-// implement sparqlFormatter
-sparqlFormatter.$inject=['snorql']
-function sparqlFormatter(snorql) {
-    var formatter;
-    function link(scope, element, attrs) {
-      scope.$watch('sparqlFormatter', function(newValue, oldValue) {
-        if (newValue){
-          formatter= snorql.SPARQLResultFormatter()
-          element.html(formatter.toDOM())
-          element.find('a').click(function(){
-            window.scrollTop();
-          })
-        }
-      });
-    }
-
-    return {
-        restrict: 'A',
-        scope: {
-            sparqlFormatter: '='
-        },
-        link: link
-    };
-}
+		return {
+			restrict: 'A',
+			scope: {
+				sparqlFormatter: '='
+			},
+			link: link
+		};
+	}
 
 })(angular);
